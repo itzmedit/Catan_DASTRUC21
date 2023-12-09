ab7295a9d20257f1379f5bf52f0f8b3757ab15f2
Diether Catan <diethercatangwapz@gmail.com>
2023-12-09 17:31:16 +0800

    DASTRUC21

---
+++ Activity 5 - Binary Tree/BinaryTreeMain.java
@@ -0,0 +1,400 @@
+// java.util package
+import java.util.Scanner;
+
+// class Node
+class Node {
+   int key;
+   Node left, right;
+
+   // Constructor
+   public Node(int item) {
+        key = item;
+        left = right = null;
+   }
+}
+
+// class BinaryTree
+public class BinaryTreeMain {
+    Node root;
+
+    // Constructor
+    BinaryTreeMain() { root = null; }
+
+    // insert method
+    Node insert(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        
+        if (key < root.key) { root.left = insert(root.left, key); } 
+        
+        else if (key > root.key) { root.right = insert(root.right, key); }
+        
+        return root;
+    }
+   
+    // printInorder method
+	void printInorder(Node node) {
+		if (node == null) { return; }
+
+		printInorder(node.left);
+		System.out.print(node.key + " ");
+		printInorder(node.right);
+	}   
+
+    // printPreorder method
+  	void printPreorder(Node node) {
+        if (node == null) { return; }
+
+        System.out.print(node.key + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+   }
+
+    // printPostorder method
+  	void printPostorder(Node node) {
+        if (node == null) { return; }
+
+        printPostorder(node.left);
+        printPostorder(node.right);
+        System.out.print(node.key + " ");
+    }
+    
+    // search method
+    Node search(Node root, int key) {
+        if (root == null || root.key == key) { return root; }
+        
+        if (key < root.key) { return search(root.left, key); }
+        
+        return search(root.right, key);
+    }
+
+    // minValue method
+    Node minValue(Node node) {
+        Node current = node;
+        
+        while (current.left != null) { current = current.left; }
+        
+        return current;
+    }
+
+    // delete method
+    Node delete(Node root, int key) {
+        if (root == null) { return root; }
+        
+        if (key < root.key) { root.left = delete(root.left, key); } 
+        
+        else if (key > root.key) { root.right = delete(root.right, key); } 
+        
+        else {
+            if (root.left == null) { return root.right; } 
+            
+            else if (root.right == null) { return root.left; }
+            
+            root.key = minValue(root.right).key;
+            root.right = delete(root.right, root.key);
+        }
+        
+        return root;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BinaryTreeMain tree = new BinaryTreeMain();
+        boolean tryProg = true;
+        String[] value = new String[1000];
+        String input;
+   
+        while (tryProg) { 
+            boolean invalid;
+            String askMain;
+            
+            // Series of numbers
+            do { // to check the input if valid or invalid
+                invalid = false;
+
+                System.out.println("\nEnter the numbers for the binary tree:");
+                input = scan.nextLine();
+
+                value = input.split(" ");
+                
+                for (String val : value) { if (invalidInput(val)) { invalid = true; } }
+
+                if (invalid) { System.out.println("\nInvalid input! Please enter again..."); }
+            
+            } while (invalid); // let the user enter again if invalid
+
+            for (String val : value) { tree.root = tree.insert(tree.root, Integer.parseInt(val)); }
+            
+            String ask = "1";
+
+            while (Integer.parseInt(ask) != 0) {
+                String ask1;
+
+                do { // to check the input if valid or invalid
+                   System.out.println("\n---> BINARY TREE <---");
+                   System.out.println("[1] TRAVERSALS \n[2] INSERT \n[3] DELETE \n[4] SEARCH \n[5] CLEAR \n[0] EXIT");
+                   ask1 = scan.nextLine().replaceAll(" ", "");
+
+                  if (invalidInputSelectA(ask1)) { System.out.println("\nInvalid input! Please select again..."); }
+         
+                } while (invalidInputSelectA(ask1)); // let the user select again if invalid
+            
+                switch (Integer.parseInt(ask1)) {
+                    // Traversals
+                    case 1: 
+                        String ask2a = "1";
+                        String ask2b;
+
+                        while (Integer.parseInt(ask2a) == 1) {
+                            do { // to check the input if valid or invalid
+                                System.out.println("\n---> TRAVERSALS <---");
+                                System.out.println("[1] INORDER \n[2] PREORDER \n[3] POSTORDER \n[0] CANCEL");
+                                ask2b = scan.nextLine().replaceAll(" ", "");
+                        
+                                if (invalidInputSelectB(ask2b)) { System.out.println("\nInvalid input! Please select again..."); }
+                        
+                            } while (invalidInputSelectB(ask2b)); // let the user select again if invalid
+
+                            switch (Integer.parseInt(ask2b)) {
+                                // Inorder Traversal
+                                case 1:
+                                    System.out.println("\nInorder traversal of the binary tree:");
+
+                                    if (tree.root != null) {
+                                        tree.printInorder(tree.root);
+                                        System.out.println();
+                                    }
+
+                                    else { System.out.println("A binary tree is empty."); }
+
+                                    break;
+                            
+                                // Preorder Traversal
+                                case 2: 
+                                    System.out.println("\nPreorder traversal of the binary tree: ");
+
+                                    if (tree.root != null) {
+                                        tree.printPreorder(tree.root);
+                                        System.out.println();
+                                    }
+
+                                    else { System.out.println("A binary tree is empty."); }
+
+                                    break;
+
+                                // Postorder Traversal
+                                case 3:
+                                    System.out.println("\nPostorder traversal of the binary tree:");
+                                    
+                                    if (tree.root != null) {
+                                        tree.printPostorder(tree.root);
+                                        System.out.println();
+                                    }
+
+                                    else { System.out.println("A binary tree is empty."); }
+
+                                    break;
+                
+                            }
+
+                            if (Integer.parseInt(ask2b) != 0) {
+                                do { // to check the input if valid or invalid
+                                    System.out.println("\nDo you want to traverse again? \n[1] Yes \n[2] No");
+                                    ask2a = scan.nextLine().replaceAll(" ", "");
+                            
+                                    if (invalidInputYN(ask2a)) { System.out.println("\nInvalid input! Please select again..."); }
+                         
+                                } while (invalidInputYN(ask2a)); // let the user select again if invalid
+                            }
+
+                            else { ask2a = "2"; }
+                        }
+
+                        break;
+                    
+                    // Insert
+                    case 2:
+                        String ask3a = "1";
+                        String ask3b;
+
+                        while (Integer.parseInt(ask3a) == 1) {
+                            do { // to check the input if valid or invalid
+                                System.out.println("\n---> INSERT <---");
+                                System.out.println("Enter a number to insert in the binary tree: ");
+                                ask3b = scan.nextLine().replaceAll(" ", "");
+
+                                if (invalidInput(ask3b)) { System.out.println("\nInvalid input! Please enter again..."); }
+                           
+                            } while (invalidInput(ask3b)); // let the user enter again if invalid
+
+                            Node find = tree.search(tree.root, Integer.parseInt(ask3b));
+
+                            if (find == null) { 
+                                tree.root = tree.insert(tree.root, Integer.parseInt(ask3b));
+                                System.out.println("\nA number has been successfully added in the binary tree.");
+                            }
+
+                            else { System.out.println("\nA number is already exist in the binary tree."); }
+
+                            do { // to check the input if valid or invalid
+                                System.out.println("\nDo you want to insert more? \n[1] Yes \n[2] No");
+                                ask3a = scan.nextLine().replaceAll(" ", "");
+                            
+                                if (invalidInputYN(ask3a)) { System.out.print("\nInvalid input! Please select again..."); }
+                         
+                            } while (invalidInputYN(ask3a)); // let the user select again if invalid
+                        }
+
+                        break;
+
+                    // Delete
+                    case 3:
+                        String ask4a = "1";
+                        String ask4b;
+
+                        while (Integer.parseInt(ask4a) == 1) {
+                            do { // to check the input if valid or invalid
+                                System.out.println("\n---> DELETE <---");
+                                System.out.println("Enter a number to delete in the binary tree: ");
+                                ask4b = scan.nextLine().replaceAll(" ", "");
+
+                                if (invalidInput(ask4b)) { System.out.println("\nInvalid input! Please enter again..."); }
+                            
+                            } while (invalidInput(ask4b)); // let the user enter again if invalid
+
+                            if (tree.root != null) {
+                                Node look = tree.search(tree.root, Integer.parseInt(ask4b));
+
+                                if (look != null) {
+                                    tree.root = tree.delete(tree.root, Integer.parseInt(ask4b));
+                                    System.out.println("\nA number has been successfully deleted in the binary tree.");
+                                }
+                            
+                                else { System.out.println("\nA number does not exist in the binary tree."); }
+                            }
+
+                            else { System.out.println("\nA binary tree is empty."); }
+
+                            do { // to check the input if valid or invalid
+                                System.out.println("\nDo you want to delete more? \n[1] Yes \n[2] No");
+                                ask4a = scan.nextLine().replaceAll(" ", "");
+                            
+                                if (invalidInputYN(ask4a)) { System.out.println("\nInvalid input! Please select again..."); }
+                         
+                            } while (invalidInputYN(ask4a)); // let the user select again if invalid
+                        }
+
+                        break;
+
+                    // Search
+                    case 4:
+                        String ask5a = "1";
+                        String ask5b;
+
+                        while (Integer.parseInt(ask5a) == 1) {
+                            do { // to check the input if valid or invalid
+                                System.out.println("\n---> SEARCH <---");
+                                System.out.println("Enter a number to search in the binary tree: ");
+                                ask5b = scan.nextLine().replaceAll(" ", "");
+
+                                if (invalidInput(ask5b)) { System.out.println("\nInvalid input! Please enter again..."); }
+                            
+                            } while (invalidInput(ask5b)); // let the user enter again if invalid
+
+                            if (tree.root != null) {
+                                Node result = tree.search(tree.root, Integer.parseInt(ask5b));
+
+                                if (result != null) { System.out.println("\nA number has been found in the binary tree."); } 
+                            
+                                else { System.out.println("\nA number has not been found in the binary tree."); }
+                            }   
+                            
+                            else { System.out.println("\nA binary tree is empty."); }
+
+                            do { // to check the input if valid or invalid
+                                System.out.println("\nDo you want to search again? \n[1] Yes \n[2] No");
+                                ask5a = scan.nextLine().replaceAll(" ", "");
+                            
+                                if (invalidInputYN(ask5a)) { System.out.println("\nInvalid input! Please select again..."); }
+                         
+                            } while (invalidInputYN(ask5a)); // let the user select again if invalid
+                        }
+                        
+                        break; 
+                        
+                    // Clear
+                    case 5:
+                        System.out.println("\n---> CLEAR <---");
+                        if (tree.root == null) { System.out.println("A binary tree is already empty."); }
+
+                        else {
+                            tree.root = null;
+                            System.out.println("A binary tree is emptied.");
+                        }
+
+                        break;
+                    
+                    // Exit
+                    case 0:
+                        ask = "0";
+                        System.out.println("\nExit...");
+                        break;
+                }
+            }
+        
+            do { // to check the input if valid or invalid
+                System.out.println("\nDo you want to try again? \n[1] Yes \n[2] No");
+                askMain = scan.nextLine().replaceAll(" ", "");
+                            
+                if (invalidInputYN(askMain)) { System.out.println("\nInvalid input! Please select again..."); }
+                         
+            } while (invalidInputYN(askMain)); // let the user select again if invalid
+
+            if (Integer.parseInt(askMain) != 1) { tryProg = false; }
+
+            else { tree.root = null; }
+        }
+
+        System.out.println("\n---> EXIT <---");
+        scan.close();
+    }
+    
+    // invalidInput method 
+    static boolean invalidInput(String _input) { 
+        boolean invalidInput = false;
+      
+        if (_input.length() == 0) { invalidInput = true; }
+        
+        else {
+            for (int x = 0; x < _input.length(); x++) {
+                char ch = _input.charAt(x);
+         
+                if (!Character.isDigit(ch)) { invalidInput = true; } 
+            }
+        }
+      
+        return invalidInput;
+    }
+   
+    // invalidInputSelectA method
+    static boolean invalidInputSelectA(String _input) {
+        return ((_input.length() != 1 || !Character.isDigit(_input.charAt(0))) || 
+            (Integer.parseInt(_input) != 0 && Integer.parseInt(_input) != 1 && Integer.parseInt(_input) != 2 && 
+            Integer.parseInt(_input) != 3 && Integer.parseInt(_input) != 4 && Integer.parseInt(_input) != 5));
+    }
+   
+    // invalidInputSelectB method
+    static boolean invalidInputSelectB(String _input) {
+        return ((_input.length() != 1 || !Character.isDigit(_input.charAt(0))) ||
+            (Integer.parseInt(_input) != 0 && Integer.parseInt(_input) != 1 && Integer.parseInt(_input) != 2 &&
+            Integer.parseInt(_input) != 3));
+    }
+
+    // invalidInputYN method
+    static boolean invalidInputYN(String _input) {
+        return ((_input.length() != 1 || !Character.isDigit(_input.charAt(0))) || (Integer.parseInt(_input) != 1 && Integer.parseInt(_input) != 2));
+    }
+}
